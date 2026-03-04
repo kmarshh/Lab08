c38f0af56a1645e673da00c137931a34b4c0b1fb
kmarshh <kpmarsh@ualberta.ca>
2026-03-03 17:28:37 -0700

    my successes!

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomListTest {
-    public class CustomListTest {
-        @Test
-        public void testHasCity() {
-            CustomList list = new CustomList();
-            City calgary = new City("Calgary", "AB");
-            list.addCity(calgary);
-            //this will fail initially because hasCity() doesn't exist
-            assertTrue(list.hasCity(calgary));
-        }
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        //this will fail initially because hasCity() doesn't exist
+        assertTrue(list.hasCity(calgary));
     }
 }
